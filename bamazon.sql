080e44d5070a4e4338a8eb2224e4d2bb9b761227
The Singer <kayla312@harbornet.com>
2018-10-27 16:34:03 -0700

    built the basic table populated with data

--- bamazon.sql
+++ bamazon.sql
@@ -1,15 +1,19 @@
-DROP DATABASE IF EXISTS !bamazon;
-CREATE database !bamazon;
+DROP DATABASE IF EXISTS bamazon;
+CREATE database bamazon;
 
-USE !bamazon;
+USE bamazon;
 
 CREATE TABLE products (
-  item_id INT NOT NULL,
+  ID INT NOT NULL AUTO_INCREMENT,
   product_name VARCHAR(100) NULL,
   department_name VARCHAR(100) NULL,
-  price DECIMAL(10,4) NULL,
+  price DECIMAL(10,2) NULL,
   stock_quantity INT (10) NULL,
-  PRIMARY KEY (item_id)
+  sold INT(10) NULL,
+  PRIMARY KEY (ID)
 );
 
-SELECT * FROM !bamazon;
+SELECT * FROM  products;
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES("Teddy", "Apparrel", 35.00, 40), ("Sex Stool", "Furniture", 49.99, 25), ("Hitatchi Wand", "Devices", 59.71, 76), ("Condoms", "Protection", 15.49, 100), ("Kama Sutra", "Books", 104.98, 9), ("Healthy Sexuality", "Books", 24.99, 47), ("Edible Underware", "Apparel", 7.99, 69), ("Dental Dam", "Protection", 15.99, 36), ("Bondage Swing", "Furniture", 69.95, 57), ("Fleshlight", "Devices", 79.95, 25);
